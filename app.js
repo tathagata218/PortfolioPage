5f3bff4f6351cbd7d377db2b1dc6e4b3d3311d0b
tathagata218 <tathagata218@yahoo.com>
2017-10-27 23:07:13 -0500

    this is the final update

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ $(function(){
 //When the right button is clicked the next iamge will populate in the div
 //whrn the left button is cilecked the previous image will be populated in the div 
 var count=0;
-var imgArray=["<a  href='#'><span align='center'><img class='img-responsive img-rounded portImages' height='400' width='700'  src='PortfolioPics/HangmanGame.jpg' alt='portfolio1'></span></a>",
-"<a href='#'><span><img class='img-responsive img-rounded portImages' width='700px' height='400px' src='PortfolioPics/JS Calculator.jpg' alt='portfolio2'></span></a>",
-"<a href='#'><span><img class='img-responsive img-rounded portImages' width='700px' height='400px' src='PortfolioPics/RPG Game.jpg' alt='portfolio3'></span></a>",
-"<a href='#'><span><img class='img-responsive img-rounded portImages' width='700px' height='400px' src='PortfolioPics/Trivia Game.jpg' alt='portfolio4'></span></a>",
-"<a href='#'><span><img class='img-responsive img-rounded portImages' width='700px' height='400px' src='PortfolioPics/WikiSearch pic.JPG' alt='portfolio5'></span></a>"
+var imgArray=["<a  href='https://tathagata218.github.io/Hangman-Game/'><span align='center'><img class='img-responsive img-rounded portImages' height='400' width='700'  src='PortfolioPics/HangmanGame.jpg' alt='portfolio1'></span></a>",
+"<a href='https://tathagata218.github.io/JS-Calculator/'><span><img class='img-responsive img-rounded portImages' width='700px' height='400px' src='PortfolioPics/JS Calculator.jpg' alt='portfolio2'></span></a>",
+"<a href='https://tathagata218.github.io/RPG-game/'><span><img class='img-responsive img-rounded portImages' width='700px' height='400px' src='PortfolioPics/RPG Game.jpg' alt='portfolio3'></span></a>",
+"<a href='https://tathagata218.github.io/Trivia-Game/'><span><img class='img-responsive img-rounded portImages' width='700px' height='400px' src='PortfolioPics/Trivia Game.jpg' alt='portfolio4'></span></a>",
+"<a href='https://tathagata218.github.io/Wikipedia-Search/'><span><img class='img-responsive img-rounded portImages' width='700px' height='400px' src='PortfolioPics/WikiSearch pic.JPG' alt='portfolio5'></span></a>"
 //"<a href='#'><span><img class='img-responsive img-rounded portImages' width='700px' height:'400px' src='PortfolioPics/HangmanGame.jpg' alt='portfolio6'></span></a>"
 ];
 var descDisplay=[" <p>Description : This is the Hangman Game and it is cool</p>",
