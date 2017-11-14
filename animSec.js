03ffaf0fe98b62e3f59e4b33b864fc340cee448a
tathagata218 <tathagata218@yahoo.com>
2017-11-13 23:30:39 -0600

    this is the last time

--- animSec.js
+++ animSec.js
@@ -2,7 +2,7 @@ var canvas = document.querySelector("canvas"),
 context = canvas.getContext("2d"),
 width = canvas.width,
 height = canvas.height,
-radius = 2.5,
+radius = 3,
 minDistance = 40,
 maxDistance = 60,
 minDistance2 = minDistance * minDistance,
@@ -30,8 +30,8 @@ for (var i = 0; i < n; ++i) {
 var p = particles[i];
 p.x += p.vx; if (p.x < -maxDistance) p.x += width + maxDistance * 2; else if (p.x > width + maxDistance) p.x -= width + maxDistance * 2;
 p.y += p.vy; if (p.y < -maxDistance) p.y += height + maxDistance * 2; else if (p.y > height + maxDistance) p.y -= height + maxDistance * 2;
-p.vx += 0.2 * (Math.random() - .5) - 0.01 * p.vx;
-p.vy += 0.2 * (Math.random() - .5) - 0.01 * p.vy;
+p.vx += 0.3 * (Math.random() - .5) - 0.01 * p.vx;
+p.vy += 0.3 * (Math.random() - .5) - 0.01 * p.vy;
 context.beginPath();
 context.arc(p.x, p.y, radius, 0, tau);
 context.fillStyle= "blue";
