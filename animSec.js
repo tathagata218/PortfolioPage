c65085c0cc39ecab68c6fd63289547941ae6e940
tathagata218 <tathagata218@yahoo.com>
2017-11-13 22:39:06 -0600

    this is the stuff

--- animSec.js
+++ animSec.js
@@ -2,15 +2,15 @@ var canvas = document.querySelector("canvas"),
 context = canvas.getContext("2d"),
 width = canvas.width,
 height = canvas.height,
-radius = 3.5,
+radius = 2.5,
 minDistance = 40,
 maxDistance = 60,
 minDistance2 = minDistance * minDistance,
 maxDistance2 = maxDistance * maxDistance;
 
 
-var tau = 4 * Math.PI,
-n = 150,
+var tau = 2 * Math.PI,
+n = 200,
 particles = new Array(n);
 
 for (var i = 0; i < n; i++) {
@@ -30,8 +30,8 @@ for (var i = 0; i < n; ++i) {
 var p = particles[i];
 p.x += p.vx; if (p.x < -maxDistance) p.x += width + maxDistance * 2; else if (p.x > width + maxDistance) p.x -= width + maxDistance * 2;
 p.y += p.vy; if (p.y < -maxDistance) p.y += height + maxDistance * 2; else if (p.y > height + maxDistance) p.y -= height + maxDistance * 2;
-p.vx += 0.3 * (Math.random() - .5) - 0.01 * p.vx;
-p.vy += 0.3 * (Math.random() - .5) - 0.01 * p.vy;
+p.vx += 0.2 * (Math.random() - .5) - 0.01 * p.vx;
+p.vy += 0.2 * (Math.random() - .5) - 0.01 * p.vy;
 context.beginPath();
 context.arc(p.x, p.y, radius, 0, tau);
 context.fillStyle= "blue";
